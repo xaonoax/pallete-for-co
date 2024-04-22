ea14c64f3fa351416b3832541bb1fa46c15d8478
Ji-0 <xaonoax@gmail.com>
2024-04-22 21:55:37 +0900

    Init: 프로젝트 세팅

---
+++ src/main/java/com/palleteforco/palleteforco/PalleteforcoApplication.java
@@ -0,0 +1,13 @@
+package com.palleteforco.palleteforco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalleteforcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalleteforcoApplication.class, args);
+	}
+
+}
