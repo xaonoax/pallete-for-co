4ce4b12dd540b50f5f600cab7f6de95bb6a257e6
Ji-0 <xaonoax@gmail.com>
2024-04-25 00:09:12 +0900

    Feat: 예외처리 기능 추가

---
+++ src/main/java/com/palleteforco/palleteforco/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.palleteforco.palleteforco.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
