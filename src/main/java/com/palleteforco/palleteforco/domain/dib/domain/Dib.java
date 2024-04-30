004536fc8b9f92670921a561663024f8fa6a7017
Ji-0 <xaonoax@gmail.com>
2024-04-30 13:20:33 +0900

    Chore: 찜 기능 필드 수정(필요 없는 항목 삭제)

--- src/main/java/com/palleteforco/palleteforco/domain/dib/domain/Dib.java
+++ src/main/java/com/palleteforco/palleteforco/domain/dib/domain/Dib.java
@@ -2,12 +2,8 @@ package com.palleteforco.palleteforco.domain.dib.domain;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class Dib {
     private Long product_id;
     private Long dib_id;
-    private Long dib_status;
-    private LocalDateTime dib_date;
 }
