104da30c6b1e115faf1d4ee7041b13bac4ad437e
Ji-0 <xaonoax@gmail.com>
2024-05-11 23:14:46 +0900

    Chore: 문의하기 코드 수정

--- src/main/java/com/palleteforco/palleteforco/domain/inquiry/domain/Inquiry.java
+++ src/main/java/com/palleteforco/palleteforco/domain/inquiry/domain/Inquiry.java
@@ -6,10 +6,12 @@ import java.time.LocalDateTime;
 
 @Data
 public class Inquiry {
+    private String email;
     private Long inquiry_id;
     private String inquiry_title;
     private String inquiry_content;
-    private LocalDateTime inquiry_regdate;
+    private String inquiry_writer;
+    private LocalDateTime inquiry_reg_date;
     private LocalDateTime inquiry_update_date;
     private String inquiry_file_attach;
 }
